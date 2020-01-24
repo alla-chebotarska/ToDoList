3dc8316a6b8c566ce997640be5cedf54affb8e19
Alla Chebotarska <alla.chebotarska@gmail.com>
2020-01-24 12:34:34 +0100

    changed user interface

--- todoItem.js
+++ todoItem.js
@@ -1,6 +1,6 @@
 Vue.component('todo-item', {
     props: ['todo'],
-    template: "<li v-bind:class='{ done: todo.isDone }'> \
+    template: "<li v-bind:class='{ done: todo.isDone }' class = 'col-md-11'> \
                     <input class='toggle' type='checkbox' v-model='todo.isDone'>{{ todo.text }} \
-               </li>"
+                    </li>"
 }) 
